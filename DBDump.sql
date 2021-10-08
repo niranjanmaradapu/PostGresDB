513b473216bb23d92c9111b511dbf2a67a105342
bbangaru <78058631+bbangaru@users.noreply.github.com>
2021-10-08 16:34:42 +0530

    Update DBDump.sql
    Added OrderID in DeliverySlip Table
    Removed unwanted lines
    Removed comments prefixed with //

--- DBDump.sql
+++ DBDump.sql
@@ -71,9 +71,6 @@ insert into userType(typeID,typeDescription) values(4,'cashier');
 insert into userType(typeID,typeDescription) values(5,'salesExecutive');
 insert into userType(typeID,typeDescription) values(6,'salesMan');
 insert into userType(typeID,typeDescription) values(7,'security');
-
-
-
 insert into userType(typeID,typeDescription) values(17,'customised');
 
 
@@ -92,10 +89,6 @@ CREATE TABLE public.userData (
     creationdate date default current_timestamp,
     lastmodified date default current_timestamp
 );
-
-1000
-
-300 retail 290(0) reatail customer 10 retail users
 
 
 insert into public.userdata(userName,password,status,phonenumber,gender,type,dateofbirth,email,domaindatauuid)
@@ -129,52 +122,13 @@ insert into userdata_av(id,ownerid,type,name,stringvalue)
 values
 ((select uniqueid from userdata where username='bhaskara'),3,'password','neerajchopra');
 
-/* these are originally discussed designed tables, but not required as introduced lookup table userType
-
-drop table if exists public.userGroup;
-CREATE TABLE public.userGroup (
-    id serial NOT NULL,
-    groupName character varying(128),
-    groupDescription character varying(256),
-    status integer,
-    uniqueID UUID default uuid_generate_v4() primary key, 
-    creationdate date default current_timestamp,
-    lastmodified date default current_timestamp
-  );
-
-drop table if exists public.userGroupUserAssignment;
-CREATE TABLE public.userGroupUserAssignment (
-    userUUID UUID references userdata(uniqueID),
-    userGroupUUID UUID references userGroup(uniqueID)
-    );
-
-drop table if exists public.userRole
-CREATE TABLE public.userRole (
-    id serial NOT NULL,
-    roleName character varying(128),
-    roleDescription character varying(256),
-    status integer,
-    uniqueID UUID default uuid_generate_v4() primary key, 
-    creationdate date default current_timestamp,
-    lastmodified date default current_timestamp
-  );
-
-
-drop table if exists public.userRoleUserAssignment;
-CREATE TABLE public.userRoleUserAssignment (
-    userUUID UUID references userData(uniqueID),
-    userRoleUUID UUID references userRole(uniqueID)
-    );
-
-*/     
-
-drop table if exists deliveryslip;//cart// multiple line items
+drop table if exists deliveryslip;
 
 CREATE TABLE public.deliveryslip (
 	dsNumber character varying(28),
 	status integer,
 	salesManID UUID references userdata(uniqueID),
-	orderid references order(uniueID),
+	orderid references order(uniqueID),
     uniqueID UUID default uuid_generate_v4() primary key,
     creationdate timestamp default current_timestamp,
     lastmodified timestamp default current_timestamp
@@ -184,10 +138,10 @@ insert into deliveryslip(dsNumber,status,salesManID) values ('ds00001',1,(select
 insert into deliveryslip(dsNumber,status,salesManID) values ('ds00002',1,(select uniqueID from userdata where username='bhaskara'));
 insert into deliveryslip(dsNumber,status,salesManID) values ('ds00003',1,(select uniqueID from userdata where username='bhaskara'));
 
-drop table if exists lineitems;//barcode//cartitem
+drop table if exists lineitems;
 CREATE TABLE public.Lineitems (
-	dsuuid UUID references deliveryslip(uniqueID),//foreign key
-	itemsku references ProductItem(uniqueID), /single product
+	dsuuid UUID references deliveryslip(uniqueID),
+	itemsku references ProductItem(uniqueID), 
 	itemprice decimal(12,2),
 	quantity integer,
 	grossvalue decimal(12,2),
@@ -227,14 +181,13 @@ drop table if exists order;
 CREATE TABLE public.order (
     orderNumber character varying(28),
     status integer, 
-    //dsUUID UUID references deliveryslip(uniqueID),//list of delivery slip
     grossvalue decimal(12,2),
     taxcode character(28),
     taxvalue decimal(12,2),
     netvalue decimal(12,2),
-    storeUUID references Stores(uniqueID)
-    customerid references userdata(uniqueID)
-    domaindatauuid references domaindata(uniqueID)
+    storeUUID references Stores(uniqueID),
+    customerid references userdata(uniqueID),
+    domaindatauuid references domaindata(uniqueID),
     uniqueID UUID default uuid_generate_v4() primary key,
     creationdate timestamp default current_timestamp,
     lastmodified timestamp default current_timestamp
@@ -259,13 +212,6 @@ CREATE TABLE public.userStoreAssignment (
     creationdate date default current_timestamp,
     lastmodified date default current_timestamp
 );
-
-1. status inclusion
-2. domain inclusion
-3. location
-
-sample data insert statements
-
 
 
 drop table if exists promotion;
@@ -385,10 +331,6 @@ insert into catalog_categories (name,description,status,parentCategoryid) values
 insert into catalog_categories (name,description,status,parentCategoryid) values ('SICO PRT','LEAF Category',1,(select uniqueID from catalog_categories where name='SAREES-LF'));
 insert into catalog_categories (name,description,status,parentCategoryid) values ('IK PRT','LEAF Category',1,(select uniqueID from catalog_categories where name='SAREES-LF'));
 
-// Gents-> main category -> i want sub category and leaf-category
-//allcategories->
-gent-> gentrousry-> cotton
-ladies-> "saree"-> silk sare
 
 drop table if exists productItem;
 
@@ -399,7 +341,7 @@ CREATE TABLE public.productItem (
     defaultimage  character varying(28), 
     stock integer,
     status integer,
-    Title/name character varying(512),
+    name character varying(512),
     costprice decimal(12,2),
     listprice decimal(12,2),
     UOM character varying(28),
@@ -409,18 +351,6 @@ CREATE TABLE public.productItem (
     creationdate timestamp default current_timestamp,
     lastmodified timestamp default current_timestamp
 );
-/*removed the catalog_categories refrences from product table and kept in barcode table;*/
-
-1,b001,10,img001.jpg,101,def001,1,'shirt ramaraj',900,1200,'piece','s001',texttile_0001,uni001,sysdate,sysdate
-2,b001,20,img001.jpg,101,def001,1,'shirt ramaraj',900,1200,'piece','s001',texttile_0001,uni002,sysdate,sysdate
-
-
-
-productvariation
-
-uni001 uni002
-uni001 uni003
-uni001 uni004
 
 
 
@@ -434,15 +364,6 @@ CREATE TABLE public.productItem_av (
     datevalue character varying(128),
     lastmodified timestamp default current_timestamp
 );
-
-/*
-color, blue, product01,
-length, 42, product01,
-productvalidty, 24th-0ct-2021, product01
-
-proudct01, 0, "color", 0 "blue",null,sysdate
-product01, 1,"lenghth",42,null,null,sysdate
-product01, 2, "productvalidity",null,null, datevalue,sysdate */
 
 
 CREATE TABLE public.productInventory
@@ -491,99 +412,3 @@ CREATE TABLE public.barcode
     creationdate timestamp default current_timestamp,
     lastmodified timestamp default current_timestamp
 );
-
-/*
-    attr_1 varchar(255) NULL,
-    attr_2 varchar(255) NULL,
-    attr_3 varchar(255) NULL,
-    attr_4 varchar(255) NULL,
-    attr_5 varchar(255) NULL,
-    attr_6 varchar(255) NULL,
-    attr_7 varchar(255) NULL,
-    attr_8 varchar(255) NULL,
-    attr_9 varchar(255) NULL,
-    attr_10 varchar(255) NULL,
-    attr_11 varchar(255) NULL,
-    attr_12 varchar(255) NULL,
-    attr_13 varchar(255) NULL,
-    attr_14 varchar(255) NULL,
-    attr_15 varchar(255) NULL,
-    attr_16 varchar(255) NULL,
-    attr_17 varchar(255) NULL,
-    attr_18 varchar(255) NULL,
-    attr_19 varchar(255) NULL,
-    attr_20 text NULL,
-    parent_barcode varchar(255) NULL,
-    cost_price numeric(15, 2) NOT NULL,
-    item_mrp numeric(15, 2) NOT NULL,
-    item_rsp numeric(15, 2) NOT NULL,
-    to_store_id int4 NOT NULL,
-    promo_label varchar(255) NULL,
-    created_at timestamp(6) NULL,
-    updated_at timestamp(6) NULL,
-    uom varchar(255) NOT NULL DEFAULT 'units'::character varying,
-    pos_hsn_master_id int4 NOT NULL DEFAULT 0,
-    original_barcode varchar(255) NULL,
-    original_barcode_created_at timestamp(6) NULL,
-    create_for_location int4 NULL DEFAULT 0,
-    value_addition_cp numeric(15, 2) NULL DEFAULT 0,
-    item_code varchar(255) NULL,
-    item_sku varchar(255) NULL,
-    attr_21 varchar(255) NULL,
-    attr_22 varchar(255) NULL,
-    attr_23 varchar(255) NULL,
-    attr_24 varchar(255) NULL,
-    attr_25 varchar(255) NULL,
-    CONSTRAINT barcode_itemsku UNIQUE (barcode, item_sku),
-    CONSTRAINT barcode_unique UNIQUE (barcode),
-    CONSTRAINT pos_inventories_pkey PRIMARY KEY (id),
-    CONSTRAINT pos_inventories_uom_check CHECK (((uom)::text = ANY (ARRAY[('units'::character varying)::text, ('meters'::character varying)::text])))
-);
-
-
-
-
-attr_1  Y   DIVISION
-attr_2  Y   SECTION
-attr_3  Y   SUBSECTION
-attr_4  Y   SUBSECTION_ID
-
-attr_10 Y   INVOICE_LINENO
-attr_11 Y   BatchNo
-attr_12 Y   Category 2
-attr_13 Y   Size
-attr_14 Y   Color
-attr_15 N   attr_15
-attr_16 N   PURCHASE TAX % - PKM
-attr_17 N   PURCHASE INV ID - PKM
-attr_18 N   V-COLOR CODE
-attr_19 N   SHORT CODE
-
-attr_20 N   attr_20
-attr_21 Y   SECOND COLOR
-attr_22 N   NOT IN USE
-attr_23 N   NOT IN USE
-attr_24 N   NOT IN USE
-attr_25 N   NOT IN USE
-
-attr_5  Y   STYLE CODE
-attr_6  N   DCODE
-attr_7  N   SID
-attr_8  N   CNSNO
-attr_9  N   INVOICENO
-
-1. Product table will not have attr columns, instead it will have the actual names defaultcategoryid
-1a. product_av
-2. Catalogcategory which is a master lookup for all categories
-3. PCA (product category assignment) join table - record wil be inserted
-4. PLP/PCP productlistprice/costprice 
-5. Inventory /stock
-
-
-Tasks.
-
-1. Alter the SQL dump file. as per Ramesh suggestions
-2. Vaguely creating the above 5 tables  
-3. Gift Voucher functionality implementation at DB level
-
-*/
